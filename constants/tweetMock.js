5fa955d082c6b9af3d44fb682ac6e5e803ff46b5
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-02 16:41:14 -0300

    feat: add usersList and tweetList

--- constants/tweetMock.js
+++ constants/tweetMock.js
@@ -1,4 +1,46 @@
-export const tweet = {
-  username: 'bobesponja',
-  tweet: 'eu amo o hub'
-};
+export const tweetList = [
+  {
+    username: 'rochelle',
+    tweet: ' Eu não preciso disso, meu marido tem dois empregos!'
+  },
+  {
+    username: 'bobesponja',
+    tweet: 'Você pode ser o que você quiser se tiver imaginação'
+  },
+  {
+    username: 'bobby',
+    tweet: 'Isso parece tão pequeno'
+  },
+  {
+    username: 'patrick',
+    tweet: 'Os pensamentos dentro da minha mente são um enigma…'
+  },
+  {
+    username: 'goku',
+    tweet: 'Eu prefiro ser um macaco sem cérebro, do que um monstro sem coração.'
+  },
+  {
+    username: 'bobesponja',
+    tweet: 'Eu gosto de encher o saco das pessoas que eu amo.'
+  },
+  {
+    username: 'bobby',
+    tweet: 'Se você não pode dizer alguma coisa de alguém, é melhor não dizer nada de bom'
+  },
+  {
+    username: 'goku',
+    tweet: 'As pessoas fortes não derrubam as outras, elas ajudam-nas a se erguerem.'
+  },
+  {
+    username: 'bulma',
+    tweet: 'O meu único pecado é ser BONITAAAA'
+  },
+  {
+    username: 'vegeta',
+    tweet: 'Enquanto o inimigo estiver na minha frente, eu lutarei.'
+  },
+  {
+    username: 'chris',
+    tweet: 'sacomé né...'
+  },
+];
--- constants/userMock.js
+++ constants/userMock.js
@@ -1,4 +1,34 @@
-export const user = {
-  username: 'bobesponja', 
-  avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info' 
-};
+export const usersList = [
+  {
+    username: 'bobesponja', 
+    avatar: 'https://static1.purebreak.com.br/articles/5/85/10/5/@/317506--bob-esponja-calca-quadrada-e-as-maiore-amp_article_image-2.jpg', 
+  },
+  {
+    username: 'patrick', 
+    avatar: 'https://static.voices.com/wp-content/uploads/2022/09/mgid_arc_imageassetref_nick.jpeg', 
+  },
+  {
+    username: 'bobby', 
+    avatar: 'https://img.elo7.com.br/product/original/1AFDD3B/painel-o-fantastico-mundo-de-bobby-g-frete-gratis-festas-infantil-tema.jpg', 
+  },
+  {
+    username: 'goku', 
+    avatar: 'https://observatoriodocinema.uol.com.br/wp-content/uploads/2018/08/Goku.jpg', 
+  },
+  {
+    username: 'vegeta', 
+    avatar: 'https://i.pinimg.com/originals/c2/65/e6/c265e608b26c9ac3edc604fb851eaaa6.jpg', 
+  },
+  {
+    username: 'bulma', 
+    avatar: 'https://ih1.redbubble.net/image.2082119931.9888/fposter,small,wall_texture,product,750x1000.jpg', 
+  },
+  {
+    username: 'chris', 
+    avatar: 'https://www.correio24horas.com.br//fileadmin/_processed_/5/0/csm_Tyler_James_Williams_todo_mundo_odeia_chris_50d033fffe.jpeg', 
+  },
+  {
+    username: 'rochelle', 
+    avatar: 'http://pm1.narvii.com/6339/cd6ead306813d8eac811cd6f58fcdec6a5a3f110_00.jpg', 
+  }, 
+];
