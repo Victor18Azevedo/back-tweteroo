6f9c463d8eff930f7988d8f50ca0cd47435dce94
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-03 13:15:36 -0300

    feat: add GET /tweets/:username

--- src/app.js
+++ src/app.js
@@ -11,15 +11,27 @@ const parseTweet = function(tweet){
   return {...tweet, avatar: tweetUser.avatar};    
 };
 
-const parseLastTweets = function (numberTweets = TWEETS_TO_SHOW) {
+const parseLastTweets = function (numberTweets = Infinity) {
   const lastTweets = tweetList.length > numberTweets ? tweetList.slice(0,numberTweets): [...tweetList];
   const tweetsPosts = lastTweets.map(tweet =>parseTweet(tweet));
   return tweetsPosts;
 };
 
+const filterUserTweet = function (username, numberTweets = Infinity) {
+  const userTweets = tweetList.filter(tweet => tweet.username === username);
+  const lastUserTweets = userTweets.length > numberTweets ? userTweets.slice(0,numberTweets): [...userTweets];
+  const userTweetsPost = lastUserTweets.map(tweet =>parseTweet(tweet));
+  return userTweetsPost;
+};
+
 app.get('/tweets',(req,res)=>{
-  console.log(parseLastTweets());
-  res.send(parseLastTweets());
+  console.log(parseLastTweets(TWEETS_TO_SHOW));
+  res.send(parseLastTweets(TWEETS_TO_SHOW));
+});
+
+app.get('/tweets/:username',(req,res)=>{
+  const username = req.params.username;
+  res.send(filterUserTweet(username, TWEETS_TO_SHOW));
 });
 
 app.listen(5000, () => console.log('Server running at port 5000...'));
