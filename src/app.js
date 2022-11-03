a501ba37fb707f3df4223c5f211febe319f57cfa
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-03 11:45:27 -0300

    feat: create parse to publish tweets

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
-import express from 'espress';
+import express from 'express';
+import { usersList } from '../constants/userMock.js';
+import { tweetList } from '../constants/tweetMock.js';
+
+const TWEETS_TO_SHOW = 10;
+
 const app = express();
+
+const parseTweet = function(tweet){
+  const tweetUser = usersList.find(u => u.username === tweet.username);
+  return {...tweet, avatar: tweetUser.avatar};    
+};
+
+const parseLastTweets = function (numberTweets = TWEETS_TO_SHOW) {
+  const lastTweets = tweetList.length > numberTweets ? tweetList.slice(0,numberTweets): [...tweetList];
+  const tweetsPosts = lastTweets.map(tweet =>parseTweet(tweet));
+  return tweetsPosts;
+};
+
+console.log(parseLastTweets());
 
 app.listen(5000, ()=>console.log('Server running at port 5000...'));
