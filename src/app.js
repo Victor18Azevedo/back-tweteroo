d7a8a43c05bb73cbc7f6beb52fa9cc77c1e68a79
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-03 21:55:15 -0300

    feat: add basic POST /sing-up

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { usersList } from '../constants/userMock.js';
 import { tweetList } from '../constants/tweetMock.js';
 
@@ -6,15 +7,20 @@ const TWEETS_TO_SHOW = 10;
 const TWEETS_PER_PAGE = 10;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-const parseTweet = function(tweet){
-  const tweetUser = usersList.find(u => u.username === tweet.username);
-  return {...tweet, avatar: tweetUser.avatar};    
+const parseTweet = function (tweet) {
+  const tweetUser = usersList.find((u) => u.username === tweet.username);
+  return { ...tweet, avatar: tweetUser.avatar };
 };
 
 const parseLastTweets = function (numberTweets = Infinity) {
-  const lastTweets = tweetList.length > numberTweets ? tweetList.slice(0,numberTweets): [...tweetList];
-  const tweetsPosts = lastTweets.map(tweet =>parseTweet(tweet));
+  const lastTweets =
+    tweetList.length > numberTweets
+      ? tweetList.slice(0, numberTweets)
+      : [...tweetList];
+  const tweetsPosts = lastTweets.map((tweet) => parseTweet(tweet));
   return tweetsPosts;
 };
 
@@ -22,29 +28,40 @@ const parsePageTweets = function (page) {
   const firsTweetIndex = TWEETS_PER_PAGE * (page - 1);
   const lastTweetIndex = firsTweetIndex + TWEETS_PER_PAGE;
   const pageTweets = tweetList.slice(firsTweetIndex, lastTweetIndex);
-  const tweetsPagePosts = pageTweets.map(tweet =>parseTweet(tweet));
+  const tweetsPagePosts = pageTweets.map((tweet) => parseTweet(tweet));
   return tweetsPagePosts;
 };
 
 const filterUserTweets = function (username, numberTweets = Infinity) {
-  const userTweets = tweetList.filter(tweet => tweet.username === username);
-  const lastUserTweets = userTweets.length > numberTweets ? userTweets.slice(0,numberTweets): [...userTweets];
-  const userTweetsPost = lastUserTweets.map(tweet =>parseTweet(tweet));
+  const userTweets = tweetList.filter((tweet) => tweet.username === username);
+  const lastUserTweets =
+    userTweets.length > numberTweets
+      ? userTweets.slice(0, numberTweets)
+      : [...userTweets];
+  const userTweetsPost = lastUserTweets.map((tweet) => parseTweet(tweet));
   return userTweetsPost;
 };
 
-app.get('/tweets',(req,res)=>{
+app.post('/sing-up', (req, res) => {
+  const { username, avatar } = req.body;
+  usersList.push({ username, avatar });
+  res.status(200).send('Ok');
+});
+
+app.get('/tweets', (req, res) => {
   if (req.query.page) {
     const page = parseInt(req.query.page);
-    page > 0 ? res.send(parsePageTweets(page)) : res.status(400).send('Informe uma página válida!');
+    page > 0
+      ? res.send(parsePageTweets(page))
+      : res.status(400).send('Informe uma página válida!');
   } else {
-    res.send(parseLastTweets(TWEETS_TO_SHOW));    
+    res.send(parseLastTweets(TWEETS_TO_SHOW));
   }
 });
 
-app.get('/tweets/:username',(req,res)=>{
+app.get('/tweets/:username', (req, res) => {
   const username = req.params.username;
   res.send(filterUserTweets(username, TWEETS_TO_SHOW));
 });
 
-app.listen(5000, () => console.log('Server running at port 5000...'));
+app.listen(5000, () => console.log('Server running in port 5000...'));
