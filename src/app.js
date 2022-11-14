6759edff5fff61018889d31512e08d9c350148ff
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-14 13:36:07 -0300

    fix: conflict in POST /sign-up

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import { usersList } from '../constants/userMock.js';
-import { tweetList } from '../constants/tweetMock.js';
+import express from "express";
+import cors from "cors";
+import { usersList } from "../constants/userMock.js";
+import { tweetList } from "../constants/tweetMock.js";
 
 const TWEETS_TO_SHOW = 10;
 const TWEETS_PER_PAGE = 10;
@@ -45,56 +45,62 @@ const filterUserTweets = function (username, numberTweets = Infinity) {
   return userTweetsPost;
 };
 
-app.post('/sign-up', (req, res) => {
+app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
   if (!username || !avatar) {
-    res.status(400).send('Todos os campos são obrigatórios!');
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
+  }
+
+  const isUserExists = usersList.find((user) => user.username === username);
+  if (isUserExists) {
+    res.status(409).send({ error: "Usuário já existente" });
     return;
   }
 
   usersList.push({ username, avatar });
-  res.status(201).send('Ok');
+  res.status(201).send("Ok");
 });
 
-app.post('/tweets', (req, res) => {
+app.post("/tweets", (req, res) => {
   const username = req.headers.user;
   const { tweet } = req.body;
   if (!username || !tweet) {
-    res.status(400).send('Todos os campos são obrigatórios!');
+    res.status(400).send({ error: "Todos os campos são obrigatórios!" });
     return;
   }
 
   tweetList.unshift({ username, tweet });
-  res.status(201).send('Ok');
+  res.status(201).send("Ok");
 });
 
-app.get('/tweets', (req, res) => {
+app.get("/tweets", (req, res) => {
   const { page } = req.query;
   if (page) {
     page > 0
       ? res.send(parsePageTweets(page))
-      : res.status(400).send('Informe uma página válida!');
+      : res.status(400).send({ error: "Informe uma página válida!" });
     return;
   }
 
   res.send(parseLastTweets(TWEETS_TO_SHOW));
 });
 
-app.get('/tweets/:username', (req, res) => {
+app.get("/tweets/:username", (req, res) => {
   const username = req.params.username;
 
   if (usersList.filter((u) => u.username === username).length === 0) {
-    res.status(400).send('Usuário inexistente');
+    res.status(400).send({ error: "Usuário inexistente" });
     return;
   }
 
   const userTweets = filterUserTweets(username);
   if (userTweets.length === 0) {
-    res.status(400).send('Usuário sem tweets');
+    res.status(400).send({ error: "Usuário sem tweets" });
     return;
   }
 
   res.send(userTweets);
 });
 
-app.listen(5000, () => console.log('Server running in port 5000...'));
+app.listen(5000, () => console.log("Server running in port 5000..."));
